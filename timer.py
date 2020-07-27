f0dca1328a09cc23059e718f1548bd8d784e0e28
aidenszeto <aszeto35@gmail.com>
2020-07-26 19:08:35 -0700

    Comments

--- timer.py
+++ timer.py
@@ -1,14 +1,18 @@
 import urllib.request, urllib.parse, urllib.error
 import json
 
+# Resets calls to 0 if worldclockapi indicates new day
 def check(current, calls):
+    # Connect with worldclockapi
     url = 'http://worldclockapi.com/api/json/est/now'
     doc = urllib.request.urlopen(url)
     data = doc.read().decode()
+    # Create JSON object with api data
     try:
         js = json.loads(data)
     except:
         js = None
+    # Reset calls to 0 if dayOfTheWeek does not match previous dayOfTheWeek
     if js['dayOfTheWeek'] != current:
         calls = 0
 
